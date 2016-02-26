6e6d0bd32b7a87b618227f4ccd413ea20dc52b03
arihandoko <ariyhandoko@gmail.com>
2016-02-26 16:05:58 +0700

    add info barcode at home

--- src/main/java/org/gkd/springwebgkd/controller/HomeController.java
+++ src/main/java/org/gkd/springwebgkd/controller/HomeController.java
@@ -7,13 +7,19 @@ import java.util.Date;
 import javax.annotation.Resource;
 
 import org.gkd.springwebgkd.bean.TagLpb;
+import org.gkd.springwebgkd.common.controller.AbstractController;
 import org.gkd.springwebgkd.service.TagLpbService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
-public class HomeController {
+public class HomeController extends AbstractController {
 	
+	public HomeController() {
+		super(HomeController.class, "home");
+		log("HomeController created.");
+	}
+
 	@Resource
 	private TagLpbService tagLpbService;
 	
@@ -32,5 +38,26 @@ public class HomeController {
 	public @ResponseBody String findName(@RequestParam(value = "npk", required = true) String npk) {
 
 		return tagLpbService.getNameNpk(npk);
+	}
+	
+	@RequestMapping(value = "/fi", method = RequestMethod.GET)
+	public @ResponseBody TagLpb findBarcodePage(@RequestParam(value = "noReg", required = true) String noReg,
+			@RequestParam(value = "kdGudang", required = true) String kdGudang) {
+
+		log("SPAG/findBarcode-GET:  ID to query = " + noReg);
+
+		TagLpb tagLpb = new TagLpb();
+		tagLpb = tagLpbService.findById(noReg);
+		if (tagLpb == null) {
+			tagLpb = new TagLpb();
+		} else {
+			Date dtperiod = tagLpbService.openPeriodeBpb();
+			BigDecimal qtyScan = tagLpbService.getQtyMutasiTag(DTFORMAT_YYYY.format(dtperiod), DTFORMAT_MM.format(dtperiod), kdGudang, noReg);
+			if (qtyScan != null){
+				tagLpb.setQtyAkhir(qtyScan);
+			}
+		}
+		
+		return tagLpb;
 	}
 }
